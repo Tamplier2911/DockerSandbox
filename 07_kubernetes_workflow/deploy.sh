a33022191d89f816833ef1c9c8378482dfaf34b2
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2020-05-22 16:43:25 +0300

    Added deployment scripts.

--- 07_kubernetes_workflow/deploy.sh
+++ 07_kubernetes_workflow/deploy.sh
@@ -1,8 +1,16 @@
-docker build -t tamplier2911/multi-client -f ./client/Dockerfile ./client
-docker build -t tamplier2911/multi-server -f ./server/Dockerfile ./server
-docker build -t tamplier2911/multi-worker -f ./worker/Dockerfile ./worker
-docker push tamplier2911/multi-client
-docker push tamplier2911/multi-server
-docker push tamplier2911/multi-worker
+docker build -t tamplier2911/multi-client:latest -t tamplier2911/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t tamplier2911/multi-server:latest -t tamplier2911/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t tamplier2911/multi-worker:latest -t tamplier2911/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push tamplier2911/multi-client:latest
+docker push tamplier2911/multi-server:latest
+docker push tamplier2911/multi-worker:latest
+
+docker push tamplier2911/multi-client:$GIT_SHA
+docker push tamplier2911/multi-server:$GIT_SHA
+docker push tamplier2911/multi-worker:$GIT_SHA
+
 kubectl apply -f k8s
-kubectl set image deployment/multi-client-deployment client=tamplier2911/multi-client
+kubectl set image deployments/multi-client-deployment client=tamplier2911/multi-client:$GIT_SHA
+kubectl set image deployments/multi-server-deployment server=tamplier2911/multi-server:$GIT_SHA
+kubectl set image deployments/multi-worker-deployment worker=tamplier2911/multi-worker:$GIT_SHA
